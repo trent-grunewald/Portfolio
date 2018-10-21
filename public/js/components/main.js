194a24f1edf03b39d61f150d0a3d238e6883f468
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-10-21 12:25:27 -0700

    nav-color change on scroll
    Updated to primary colors
    
    added nav color change on scroll

--- public/js/components/main.js
+++ public/js/components/main.js
@@ -29,35 +29,39 @@
     })
   }
 
-
   // SMOOTH SCROLL
   function smoothScroll(target, duration) {
+    //target arguement variable
     let userTarget = document.querySelector(target);
+    //gets the top coordinates for the users target section
     let targetPosition = userTarget.getBoundingClientRect().top;
+    //takes into account the users starting page position.
     let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
+    //will keep track of the time for requestAnimationFrame
     let startTime = null;
 
     function animation(currentTime){
       if(startTime === null)startTime = currentTime;
       let timeElapsed = currentTime - startTime;
-      const run = ease(timeElapsed, startPosition, distance, duration);
+      //runs the animation and subtracts 72px for the navbar
+      const run =  ease(timeElapsed, startPosition, targetPosition, duration) -72;
+      //scrolls to the Y position for the Run function (first (0) is X)
       window.scrollTo(0, run);
       if(timeElapsed < duration) requestAnimationFrame(animation);
       
     }
-
+    // Im not going to lie, the math below doesn't make sense to me, I found it online
     function ease(t, b, c, d) {
       t /= d / 2;
       if (t <1) return c / 2 * t * t + b;
       t--;
       return -c / 2 * (t * (t -2) -1) + b;
     }
-
-
+    // loops through through the animation function at 60fps
     requestAnimationFrame(animation);
   }
 
+  //YOU ONLY NEED TO CHANGE / ADD TO THESE TO ENABLE IT IN THE PAGE
   const about = document.querySelector('.aboutTarget');
   about.addEventListener('click', function(){
     smoothScroll('#about',1000);
@@ -72,7 +76,32 @@
   })
 
 
+  function scrollColor(){
+    const navColor = document.querySelector('.nav');
+    window.onscroll = function(){
+      if(scrollTop > 72) {
+        navColor.classList.addClass('changeColor')
+      }
+      if(scrollTop < 72) {
+        navColor.removeClass('changeColor')
+      }
+    }
+  }
 
+  const navColor = document.querySelector('.nav');
+  window.onscroll =_=>{ 
+      "use strict";
+      if (document.body.scrollTop >= 200 ) {
+          navColor.classList.add("changeColor");
+          navColor.classList.remove("navBack");
+      } 
+      else {
+          navColor.classList.remove("changeColor");
+          navColor.classList.add("navBack");
+      }
+  };
+
+//CRABBY CAT FOOTER CLICK ---- THE MOST IMPORTANT PART OF THE PORTFOLIO!
   const footerClick = document.getElementById('what');
   function alertFunc(){
   const footerBG = document.getElementById('bg');
